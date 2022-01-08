b4b0e95f8f586d11ef2d7edeacba21826dbe2a38
unknown <darius.kloda@gmail.com>
2022-01-08 07:18:28 +0100

    second

--- components/organisms/SecondPage.tsx
+++ components/organisms/SecondPage.tsx
@@ -7,31 +7,42 @@ export default function SecondPage(): JSX.Element {
     'textSizes font-bold text-pageWhite': true
   });
   return (
-    <section className="bg-pageDarkGrey flex flex-col justify-center items-start h-[100vh]">
-      <div className="relative">
-        <HeaderElement className="flex flex-col justify-center items-start absolute bottom-2">
+    <section className="bg-pageWhite">
+      <div className="flex justify-center items-end lg:items-center relative">
+        <HeaderElement className="flex flex-col justify-center items-start absolute md:static bottom-2">
           <TextElement
-            className="textSizes font-bold text-pageWhite"
+            className="text-5xl md:text-6xl lg:text-8xl font-bold text-pageWhite md:text-pageBlack"
             htmlTag="h3"
           >
             CAMPAIGNS
           </TextElement>
-          <TextElement className={TextHClasses} htmlTag="h3">
+          <TextElement
+            className="text-5xl md:text-6xl lg:text-8xl font-bold text-pageWhite md:text-pageBlack"
+            htmlTag="h3"
+          >
             MARKETING
           </TextElement>
-          <TextElement className={TextHClasses} htmlTag="h3">
+          <TextElement
+            className="text-5xl md:text-6xl lg:text-8xl font-bold text-pageWhite md:text-pageBlack"
+            htmlTag="h3"
+          >
             STRATEGY
           </TextElement>
           <TextElement
-            className="text-1xl md:text-8xl lg:text-9xl mt-4 text-pageWhite"
+            className="text-1xl md:text-3xl lg:text-3xl my-4 text-pageWhite md:text-pageBlack break-words sm:w-3/4 md:w-full lg:w-[90%]"
             htmlTag="p"
           >
             Consectetuer adipiscing elit, sed diam nibh euismod tincidunt ut
             laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim
-            veniam, quis nostrud .{' '}
+            veniam, quis nostrud&nbsp;.{' '}
           </TextElement>
         </HeaderElement>
-        <video loop={true} autoPlay={true}>
+        <video
+          className="w-full md:w-1/2 "
+          loop={true}
+          autoPlay={true}
+          muted={true}
+        >
           <source src="woman.mp4" type="video/mp4"></source>
         </video>
       </div>
